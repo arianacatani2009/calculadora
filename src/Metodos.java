be2ec64ee5945585c1cc17933854300b22ec9b6c
arianacatani2009 <mayerlicatani@gmail.com>
2023-07-11 16:05:37 -0500

    Ariana calcu

--- src/Metodos.java
+++ src/Metodos.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Metodos {
     public JPanel panel;
     private String cadena;
